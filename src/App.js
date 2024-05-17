30455623f891ed5e5cae09787db5b7499a94e090
Sander Nobel <sander.nobel@protonmail.com>
2024-05-17 20:20:47 +0200

    initial quiz app finalized

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
 
-  const colors = ['red', 'blue', 'green', 'pink'];
+  const colors = ['red', 'blue', 'green', 'purple'];
   const parsedData = JSON.parse(JSON.stringify(data));
   const questions = parsedData.questions.map(question => ({
     ...question,
@@ -14,6 +14,7 @@ function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
+  const [started, setStarted] = useState(false);
 
   const handleAnswerOptionClick = (isCorrect) => {
     if (isCorrect) {
@@ -30,15 +31,24 @@ function App() {
 
   return (
     <div className='app'>
-      <h1>Baby Quiz</h1>
-      {showScore ? (
-        <div className='score-section'>You scored {score} out of {questions.length}</div>
+      <h1>{parsedData.title}</h1>
+      { !started ? (<div className='container'>
+        <img src={require('./images/1.png')} alt='quiz' />  
+        <button onClick={()=> setStarted(true)}>Start Quiz</button>
+      <h3>{parsedData.description}</h3>
+</div>) :(
+      <div>{showScore ? (
+        <div className='score-section'>
+        <div>You scored {score} out of {questions.length}</div>
+        <button onClick={() => window.location.reload()}>Restart Quiz</button>
+        </div>
       ) : (
         <>
           <div className='question-section'>
             <div className='question-count'>
               <span>Question {currentQuestion + 1}</span>/{questions.length}
             </div>
+            <img src={require(`./images/${currentQuestion + 2}.png`)} alt='quiz' className='question-image' />
             <div className='question-text'>{questions[currentQuestion].question}</div>
           </div>
           <div className='answer-section'>
@@ -53,7 +63,7 @@ function App() {
   ))}
 </div>
         </>
-      )}
+      )}</div>)}
     </div>
   );
 }
